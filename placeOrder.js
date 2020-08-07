ffd2e5a6c8091bce36532de4ac581224a42cbc8f
allan <allan@funrock.com>
2020-08-07 13:02:33 +0200

    initial commit

---
+++ placeOrder.js
@@ -0,0 +1,28 @@
+var urlParams = new URLSearchParams(window.location.search);
+
+setTimeout(function () {
+    var e = window.document.getElementById("buyVolume");
+    if (e === null) {
+        e = window.document.getElementById("sellVolume");
+    }
+
+    var volume = parseInt(urlParams.get('volume'));
+    if(!isNaN(volume)){
+        e.value = volume;
+        e.dispatchEvent(new Event('input', { 'bubbles': true }));
+        e.dispatchEvent(new Event('keyup', { 'bubbles': true }));
+    }
+    
+
+    e = window.document.getElementById("buyPrice");
+    if (e === null) {
+        e = window.document.getElementById("sellPrice");
+    }
+
+    var price = parseInt(urlParams.get('price'));
+    if(!isNaN(price)){
+        e.value = price;
+        e.dispatchEvent(new Event('input', { 'bubbles': true }));
+        e.dispatchEvent(new Event('keyup', { 'bubbles': true }));
+    }
+}, 1000);
